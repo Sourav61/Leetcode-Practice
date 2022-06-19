aa1ddc1448d5d7447af898f7f566b83072ad877a
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2022-06-19 10:56:18 +0530

    Time: 58 ms (27.05%), Space: 30.8 MB (63.22%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -12,10 +12,27 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        
         if(root == NULL){
             return 0;
         }
+        
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {
+            hl++;
+            l=l->left;
+        }
+
+        while(r) {
+            hr++;
+            r=r->right;
+        }
+
+        if(hl==hr) 
+            return pow(2,hl)-1;
+
 
         return countNodes(root->left) + countNodes(root->right) + 1;
 
