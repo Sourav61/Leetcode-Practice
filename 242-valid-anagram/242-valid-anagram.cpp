a90e6e7b8eb0d085b727b70adf0483cf77e05919
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2022-06-29 21:44:24 +0530

    Time: 26 ms (13.13%), Space: 7.2 MB (77.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.length() == t.length()){
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+
+            return s==t;
+        }
         
-        return s==t;
+        return false;
     }
 };
